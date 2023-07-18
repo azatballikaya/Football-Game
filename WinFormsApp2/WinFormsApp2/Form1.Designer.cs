f23b2843a5916254d640112c1a10ba6c2f1e48f8
Azat <azat.bal@hotmail.com>
2023-07-18 19:10:20 +0300

    C# Football Game

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label_fbpuan = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label_gspuan = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label_bjkpuan = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label_tspuan = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Blue;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(486, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Blue;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 41);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Blue;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(486, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 41);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Oyna";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Red;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(307, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 31);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Red;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(307, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 31);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Yellow;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(210, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 38);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Yellow;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(210, 151);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 38);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Yellow;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(399, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 38);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Yellow;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(399, 148);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(106, 38);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Red;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(544, 260);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(112, 28);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Fenerbahçe";
+            // 
+            // label_fbpuan
+            // 
+            this.label_fbpuan.AutoSize = true;
+            this.label_fbpuan.BackColor = System.Drawing.Color.Red;
+            this.label_fbpuan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_fbpuan.Location = new System.Drawing.Point(668, 260);
+            this.label_fbpuan.Name = "label_fbpuan";
+            this.label_fbpuan.Size = new System.Drawing.Size(23, 28);
+            this.label_fbpuan.TabIndex = 14;
+            this.label_fbpuan.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Red;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label14.Location = new System.Drawing.Point(544, 305);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(113, 28);
+            this.label14.TabIndex = 15;
+            this.label14.Text = "Galatasaray";
+            // 
+            // label_gspuan
+            // 
+            this.label_gspuan.AutoSize = true;
+            this.label_gspuan.BackColor = System.Drawing.Color.Red;
+            this.label_gspuan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_gspuan.Location = new System.Drawing.Point(668, 305);
+            this.label_gspuan.Name = "label_gspuan";
+            this.label_gspuan.Size = new System.Drawing.Size(23, 28);
+            this.label_gspuan.TabIndex = 16;
+            this.label_gspuan.Text = "0";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Red;
+            this.label16.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label16.Location = new System.Drawing.Point(544, 353);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(81, 28);
+            this.label16.TabIndex = 17;
+            this.label16.Text = "Beşiktaş";
+            // 
+            // label_bjkpuan
+            // 
+            this.label_bjkpuan.AutoSize = true;
+            this.label_bjkpuan.BackColor = System.Drawing.Color.Red;
+            this.label_bjkpuan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_bjkpuan.Location = new System.Drawing.Point(668, 353);
+            this.label_bjkpuan.Name = "label_bjkpuan";
+            this.label_bjkpuan.Size = new System.Drawing.Size(23, 28);
+            this.label_bjkpuan.TabIndex = 18;
+            this.label_bjkpuan.Text = "0";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Red;
+            this.label18.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label18.Location = new System.Drawing.Point(544, 402);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(120, 28);
+            this.label18.TabIndex = 19;
+            this.label18.Text = "Trabzonspor";
+            // 
+            // label_tspuan
+            // 
+            this.label_tspuan.AutoSize = true;
+            this.label_tspuan.BackColor = System.Drawing.Color.Red;
+            this.label_tspuan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_tspuan.Location = new System.Drawing.Point(668, 402);
+            this.label_tspuan.Name = "label_tspuan";
+            this.label_tspuan.Size = new System.Drawing.Size(23, 28);
+            this.label_tspuan.TabIndex = 20;
+            this.label_tspuan.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(663, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(210, 353);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 20);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "label13";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label_tspuan);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label_bjkpuan);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label_gspuan);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label_fbpuan);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label_fbpuan;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label_gspuan;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label_bjkpuan;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label_tspuan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int sayac = 0;
+        int gspuan = 0, fbpuan = 0, tspuan = 0, bjkpuan = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            sayac++;
+            
+            if (sayac == 1)
+            {
+                int fbgol=rastgele.Next(0,6), gsgol= rastgele.Next(0, 6), bjkgol= rastgele.Next(0, 6), tsgol= rastgele.Next(0, 6);
+                
+                label1.Text = "1. Hafta Macları";
+                label2.Text = "Fenerbahçe";
+                label3.Text = "Beşiktaş";
+                label4.Text = "Galatasaray";
+                label5.Text = "Trabzonspor";
+
+                label8.Text = fbgol.ToString();
+                label10.Text = bjkgol.ToString();
+                label9.Text=gsgol.ToString  ();
+                label11.Text=tsgol.ToString ();
+
+                if (fbgol > bjkgol)
+                {
+                    fbpuan += 3;
+                    label_fbpuan.Text = fbpuan.ToString();
+                }
+                else if (bjkgol > fbgol)
+                {
+                    bjkpuan += 3;
+                    label_bjkpuan.Text=bjkpuan.ToString();
+                }
+                else
+                {
+                    fbpuan++;
+                    bjkpuan++;
+                    label_fbpuan.Text = fbpuan.ToString();
+                    label_bjkpuan.Text = bjkpuan.ToString();
+
+                }
+
+                if (gsgol > tsgol)
+                {
+                    gspuan += 3;
+                    label_gspuan.Text =(gspuan.ToString());
+
+                }
+                else if (tsgol > gsgol)
+                {
+                    tspuan += 3;
+                    label_tspuan.Text=tspuan.ToString();
+                }
+                else
+                {
+                    tspuan++;
+                 
+                    gspuan++;
+                    label_gspuan.Text = (gspuan.ToString());
+                    label_tspuan.Text = tspuan.ToString();
+                }
+
+
+            }
+
+            if (sayac == 2)
+            {
+                int fbgol = rastgele.Next(0, 6), gsgol = rastgele.Next(0, 6), bjkgol = rastgele.Next(0, 6), tsgol = rastgele.Next(0, 6);
+                
+                //ts-fb
+                //gs-bjk
+                label1.Text = "2. Hafta Macları";
+                label2.Text = "Trabzonspor";
+                label3.Text = "Fenerbahçe";
+                label4.Text = "Galatasaray";
+                label5.Text = "Beşiktaş";
+
+                label8.Text = tsgol.ToString();
+                label10.Text = fbgol.ToString();
+                label9.Text = gsgol.ToString();
+                label11.Text=bjkgol.ToString ();
+
+                if (tsgol > fbgol)
+                {
+                    tspuan += 3;
+                    label_tspuan.Text = tspuan.ToString();
+                }
+                else if (fbgol > tsgol)
+                {
+
+                    fbpuan+=3;
+                    label_fbpuan.Text = fbpuan.ToString();
+
+                }
+                else
+                {
+                    fbpuan++;
+                    tspuan++;
+                    label_fbpuan.Text = fbpuan.ToString();
+                    label_tspuan.Text=tspuan.ToString();
+
+                }
+
+                if (gsgol > bjkgol)
+                {
+
+                    gspuan += 3;
+                    label_gspuan.Text = gspuan.ToString();
+
+                }
+                else if (bjkgol > gsgol)
+                {
+                    bjkpuan+=3;
+                    label_bjkpuan.Text = bjkpuan.ToString();
+
+                }
+                else
+                {
+                    bjkpuan++;
+                    gspuan++;
+                    label_bjkpuan.Text = bjkpuan.ToString();
+                    label_gspuan.Text = gspuan.ToString();
+
+                }
+                
+            }
+
+            if (sayac == 3)
+            {
+                label1.Text = "3. Hafta Maçları";
+                int fbgol = rastgele.Next(0, 6), gsgol = rastgele.Next(0, 6), bjkgol = rastgele.Next(0, 6), tsgol = rastgele.Next(0, 6);
+
+                //fb-gs
+                //bjk-ts
+
+                label2.Text = "Fenerbahçe";
+                label3.Text = "Galatasaray";
+                label4.Text = "Beşiktaş";
+                label5.Text = "Trabzonspor";
+
+                label8.Text = fbgol.ToString();
+                label10.Text = gsgol.ToString();
+                label9.Text = bjkgol.ToString();
+                label11.Text = tsgol.ToString();
+
+                if (fbgol > gsgol)
+                {
+                    fbpuan += 3;
+                    label_fbpuan.Text = fbpuan.ToString();
+
+                }
+                else if (gsgol > fbgol)
+                {
+
+                    gspuan += 3;
+                    label_gspuan.Text = gspuan.ToString();
+
+                }
+                else
+                {
+                    gspuan++;
+                    fbpuan++;
+                    label_gspuan.Text=gspuan.ToString();
+                    label_fbpuan.Text=fbpuan.ToString();
+
+                }
+
+
+                if (bjkgol > tsgol)
+                {
+
+                    bjkpuan += 3;
+                    label_bjkpuan.Text = bjkpuan.ToString();
+
+                }
+                else if (tsgol > bjkgol)
+                {
+                    tspuan += 3;
+                    label_tspuan.Text = tspuan.ToString();
+
+                }
+                else
+                {
+                    tspuan++;
+                    bjkpuan++;
+                    label_tspuan.Text=tspuan.ToString();
+                    label_bjkpuan.Text = bjkpuan.ToString();
+
+                }
+
+                if (fbpuan > gspuan && fbpuan > bjkpuan && fbpuan > tspuan)
+                {
+                    label13.Text = "ŞAMPİYON FENERBAHÇE";
+                    pictureBox1.ImageLocation = @"C:\Users\Azat\Desktop\takım resimleri\fb.jpg";
+
+                }
+                else if (gspuan > fbpuan && gspuan > bjkpuan && gspuan > tspuan)
+                {
+                    label13.Text = "ŞAMPİYON GALATASARAY";
+                    pictureBox1.ImageLocation = @"C:\Users\Azat\Desktop\takım resimleri\gs.jpg";
+
+                }
+                else if (bjkpuan > gspuan && bjkpuan > fbpuan && bjkpuan > tspuan)
+                {
+                    label13.Text = "ŞAMPİYON BEŞİKTAŞ";
+                    pictureBox1.ImageLocation = @"C:\Users\Azat\Desktop\takım resimleri\bjk.jpg";
+                }
+                else if (tspuan > fbpuan && tspuan > gspuan && tspuan > bjkpuan)
+                {
+                    label13.Text = "ŞAMPİYON TRABZONSPOR";
+                    pictureBox1.ImageLocation = @"C:\Users\Azat\Desktop\takım resimleri\ts.jpg";
+
+                }
+                button1.Text = "Lig Bitti";
+                button1.Enabled = false;
+
+
+
+            }
+            
+               
+            
+
+        }
+    }
+}
